291eab0b453a88b9b49bd12f0131775f2bad1d15
mahabeer1997 <mattmahabeer@outlook.com>
2022-07-26 13:47:48 -0500

    Delete Handler

--- pages/api/apiHandler.js
+++ pages/api/apiHandler.js
@@ -3,3 +3,4 @@ import { node } from "./baseUrl";
 export const getBrands = async () => await node.get("/brands").then((res) => res.data);
 export const getModels = async (brandId) => await node.get(`/models/${brandId}`).then((res) => res.data);
 export const addBrand = async (data) => await node.post("/brands", data);
+export const deleteBrand = async (brandId) => await node.delete(`/brands/${brandId}`).then((res) => res.data);
--- pages/machines/createx.jsx
+++ pages/machines/createx.jsx
@@ -1,40 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styles from "../../styles/Machine.module.css";
 import Link from "next/link";
-import { useQuery, QueryErrorResetBoundary } from '@tanstack/react-query';
-import { getBrands, getModels } from '../api/apiHandler';
-import BrandModelListing, {selectedBrandAtom} from './components/BrandModelListing';
+import { useQuery, QueryErrorResetBoundary, useMutation } from '@tanstack/react-query';
+import { getBrands, getModels, deleteBrand } from '../api/apiHandler';
+import BrandModelListing, { selectedBrandAtom } from './components/BrandModelListing';
 import { useAtom } from 'jotai';
 import CircularProgress from '@mui/material/CircularProgress'
 import { useSpring, animated, config } from 'react-spring';
 import { isEmpty } from 'lodash';
-
+import { useCallback } from 'react';
+import Delete from "../../componentsx/machine-components/modals/Delete";
 
 function CreateX() {
   const [brandAtom] = useAtom(selectedBrandAtom);
   const [selectedBrand, setSelectedBrand] = useState(null);
   const [selectedModel, setSelectedModel] = useState(null);
+  const [operator, setOperator] = useState("");
+  const [brandIdProp, setBrandIdProp] = useState(null);
 
-  const { isLoading: brandsLoading, data: brands, error: brandsError } = useQuery(['brands'], getBrands);
-  const { isLoading: modelsLoading, data: models, error: modelsError, refetch: refetchModels } = useQuery(['models'], () => getModels(selectedBrand?.id), {
-    enabled: selectedBrand !== null,
+
+  const { isLoading: brandsLoading, data: brands, error: brandsError, refetch: refetchBrands } = useQuery(['brands'], getBrands);
+  const { isLoading: modelsLoading, isFetching: modelsFetching,
+    isSuccess: modelsSuccess,
+    data: models, error: modelsError, refetch: refetchModels } = useQuery(['models'], () => getModels(selectedBrand?.id), {
+      enabled: selectedBrand !== null,
+    });
+  const { data: deleteBrandReturn, isLoading: deleteBrandLoading, isSuccess: deleteBrandSuccess, mutate: deleteBrand } = useMutation(() => deleteBrand(brandId), {
+    onSuccess: refetchBrands,
   });
+
+  const deleteRef = useRef();
+
+  const toggleRefState = useCallback(() => {
+    deleteRef.current.toggleModal();
+  }, [deleteRef]);
+
+  const toggleR = {
+    deleteBrand: useCallback(() => {
+      deleteRef.current.toggleModal();
+      setOperator("brand");
+    }, [deleteRef]),
+    deleteModel: useCallback(() => {
+      deleteRef.current.toggleModal();
+      setOperator("model");
+    }, [deleteRef])
+  } 
 
   const animation = useSpring({
     config: { ...config.stiff },
     from: { opacity: 0 },
     to: { opacity: 1 }
-});
+  });
 
- function getModel(brand){
-  if(selectedBrand == null) {
-    setSelectedBrand(brand);
-    return;
-  } else {
-    setSelectedBrand(brand);
-    refetchModels()
+  function getModel(brand) {
+    if (selectedBrand == null) {
+      setSelectedBrand(brand);
+      return;
+    } else {
+      setSelectedBrand(brand);
+      refetchModels()
+    }
   }
- }
+
+  const deleteProps = (brandId) => {
+    brandDeleteProps: {
+      brandId: brandIdProp,
+      deleteFunction: deleteBrand(brandId),
+    }
+  }
+
 
   console.log(brands);
   return (
@@ -55,78 +89,98 @@ function CreateX() {
           </div>
           <hr className={styles.linebreak} />
           <div>
-            {brandsLoading && 
-            <div style={{margin: '0 40%'}}>
-              <CircularProgress />
-            </div> }
-            {brandsError && <div style={{margin: 'auto'}}>
+            {brandsLoading &&
+              <div style={{ margin: '0 40%' }}>
+                <CircularProgress />
+              </div>}
+            {brandsError && <div style={{ margin: 'auto' }}>
               There was an error retreiving brands from the database
             </div>}
-            {!brandsLoading && !brandsError && 
-            (!isEmpty(brands) && selectedBrand == null ? 
-            <animated.div style={animation}>
-              <div className={styles.brandlistcontainer}>
-              {brands.map((brand) => {
-                return (
-                  <button key={brand.id} className={styles.brandlistitem} onClick={() => getModel(brand)}>
-                    <div className={styles.brand}>
-                      {brand.name}
-                    </div>
-                  </button>
-                  
-                )
-              })}
-              </div>
-            </animated.div> : isEmpty(brands) ? 
-            <div style={{margin: 'auto'}}>
-              No brands found in the database.
-            </div> : 
-            !isEmpty(brands) && selectedBrand !== null ? 
-            <animated.div>
-                    <div className={styles.brandlistcontainer}>
+            {!brandsLoading && !brandsError &&
+              (!isEmpty(brands) && selectedBrand == null ?
+                <animated.div style={animation}>
+                  <div className={styles.brandlistcontainer}>
+                    {brands.map((brand) => {
+                      return (
+                        <button key={brand.id} className={styles.brandlistitem} onClick={() => getModel(brand)}>
+                          <div className={styles.brand}>
+                            {brand.name}
+                          </div>
+                        </button>
+
+                      )
+                    })}
+                  </div>
+                </animated.div> : isEmpty(brands) ?
+                  <div style={{ margin: 'auto' }}>
+                    No brands found in the database.
+                  </div> :
+                  !isEmpty(brands) && selectedBrand !== null ?
+                    <animated.div>
+                      <div className={styles.brandlistcontainer}>
                         <button className={styles.brandlistitem}>
-                            <div className={styles.brand}>
-                                {selectedBrand?.name}
-                            </div>
+                          <div className={styles.brand}>
+                            {selectedBrand?.name}
+                          </div>
                         </button>
                         <button
-                            onClick={() => setSelectedBrand(null)}
-                            className={styles.clearbrandstate}
+                          onClick={() => setSelectedBrand(null)}
+                          className={styles.clearbrandstate}
                         >
-                            Cancel
+                          Cancel
                         </button>
-                    </div>
-                </animated.div> : ''
-            )
-             }
+                        <button onClick={(toggleR.deleteBrand)} className={styles.deletebrandbutton}>
+                          Delete Brand
+                        </button>
+                      </div>
+                    </animated.div> : ''
+              )
+            }
           </div>
         </div>
-        {selectedBrand !== null && (
-          modelsLoading ? <div style={{margin: '0 40%'}}>
-          <CircularProgress />
-        </div> : 
-        isEmpty(models) && !modelsError ? 
-        <div className={styles.brandtitle}>
-                                    No associated models were found for this brand.
-                                </div> : 
-        !isEmpty(models) ? 
-        <animated.div>
-        <div className={styles.brandlistcontainer}>
-                                {models?.map((model) => {
-                                    return (
-                                        <button key={model.id} className={styles.brandlistitem}>
-                                            <div className={styles.brand}>
-                                                {model.name}
-                                            </div>
-                                        </button>
-                                    )
-                                })}
-                            </div>
-                            </animated.div>
-                             : ''
-        )
+        {selectedBrand !== null &&
+          <div>
+            <div className={styles.brandline}>
+              <div className={styles.titleline}>
+                <div className={styles.brandtitle}>Models</div>
+                <button className={styles.addbrandbutton}>
+                  Add a model for {selectedBrand?.name}
+                </button>
+              </div>
+              <hr className={styles.linebreak} />
+              {
+                modelsLoading || modelsFetching ?
+                  <div style={{ margin: '0 30%' }}>
+                    <CircularProgress />
+                  </div>
+                  :
+                  (!modelsLoading || !modelsFetching) && (modelsSuccess && !isEmpty(models)) ?
+                    <animated.div>
+                      <div className={styles.brandlistcontainer}>
+                        {models?.map((model) => {
+                          return (
+                            <button key={model.id} className={styles.brandlistitem}>
+                              <div className={styles.brand}>
+                                {model.name}
+                              </div>
+                            </button>
+                          )
+                        })}
+                      </div>
+                    </animated.div>
+                    : (!modelsLoading || !modelsFetching) && (modelsSuccess && isEmpty(models)) ?
+                      <animated.div>
+                        <div className={styles.brandtitle}>
+                          No associated models were found for this brand
+                        </div>
+                      </animated.div>
+                      : ''
+              }
+            </div>
+          </div>
         }
       </div>
+      <Delete ref={deleteRef} operator={operator} />
     </React.Fragment>
   );
 }
